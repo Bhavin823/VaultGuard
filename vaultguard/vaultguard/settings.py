7ecc027857b913ad985d8a7b6d590aea21e9ee24
Bhavin Chavda <bhavinchavada242846@gmail.com>
2024-06-23 19:24:26 +0530

    add whitenoise library and configure whitenoise middleware, and add at end of seetings  whitenoise config code

--- vaultguard/vaultguard/settings.py
+++ vaultguard/vaultguard/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-o+u=%+(myc*2+j=a1c#&ql7ttl!p9vf-0l+q=0&qth1qk^d3d!
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['VaultGuard.pythonanywhere.com']
+ALLOWED_HOSTS = ['VaultGuard.pythonanywhere.com','127.0.0.1']
 
 
 # Application definition
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -135,3 +136,11 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 SESSION_COOKIE_AGE = 900
 
 
+# Static file serving.
+# https://whitenoise.readthedocs.io/en/stable/django.html#add-compression-and-caching-support
+STORAGES = {
+    # ...
+    "staticfiles": {
+        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+    },
+}
