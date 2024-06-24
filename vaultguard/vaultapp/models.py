d11352badb2444479873038db1fa93451ae13b85
Bhavin Chavda <bhavinchavada242846@gmail.com>
2024-06-24 19:46:43 +0530

    make some changes then undo changes in models and view about saving password

--- vaultguard/vaultapp/models.py
+++ vaultguard/vaultapp/models.py
@@ -1,5 +1,21 @@
 from django.db import models
 from django.contrib.auth.models import User
+from cryptography.fernet import Fernet
+import base64
+from django.conf import settings
+
+def encrypt_passowrd(password):
+    secret_key = settings.SECRET_KEY.encode()
+    fernet = Fernet(secret_key)
+    encrypted_passowrd = fernet.encrypt(password.encode()).decode()
+    return encrypted_passowrd
+
+def decrypt_password(encrypted_passowrd):
+    secret_key = settings.SECRET_KEY.encode()
+    fernet = Fernet(secret_key)
+    decrypted_password = fernet.decrypt(encrypted_passowrd.encode()).decode()
+    return decrypted_password
+
 
 # Create your models here.
 class PasswordVault(models.Model):
@@ -7,7 +23,7 @@ class PasswordVault(models.Model):
     website = models.CharField(max_length=255)
     username_of_website = models.CharField(max_length=255)
     password_of_website = models.CharField(max_length=255)
-
+    
     def __str__(self):
         return f"{self.user.username}'s Password For {self.website}"
     
--- vaultguard/vaultapp/views.py
+++ vaultguard/vaultapp/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render,redirect
 from .models import PasswordVault
 from django.contrib.auth.decorators import login_required
 
+
 # Create your views here.
 # @login_required(login_url='login')
 def passwordListView(request):
